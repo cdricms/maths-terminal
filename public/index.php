1c84568cc9f6247b7764176fab7e150e6497cb37
Smoqu <36056008+Smoqu@users.noreply.github.com>
2021-12-24 14:28:24 +0100

    Bug fix: Sidebar and button not reacting properly

--- public/index.php
+++ public/index.php
@@ -6,7 +6,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <script defer src="./index.js"></script>
     <link rel="stylesheet" href="style.css">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
       rel="stylesheet">
@@ -126,4 +125,63 @@
     <!-- End/content -->
     </main>
 </body>
+<script>
+    const hiddenItems = document.getElementsByClassName("hidden");
+
+    for (let x = 0; x < hiddenItems.length; x++) {
+    const hi = hiddenItems[x];
+    hi.style.visibility = "hidden";
+
+    const parent = hi.parentNode;
+
+    const button = document.createElement("span");
+    button.className = "material-icons";
+    button.id = "eye";
+    button.textContent = "visibility_off";
+    button.title = "Montrer la réponse.";
+    button.onclick = () => {
+        hi.style.visibility = hi.style.visibility === "hidden" ? "" : "hidden";
+        button.textContent =
+        hi.style.visibility === "hidden" ? "visibility_off" : "visibility";
+    };
+
+    const wrapper = document.createElement("section");
+    wrapper.className = "wrapper-hidden";
+    parent.replaceChild(wrapper, hi);
+    wrapper.appendChild(hi);
+
+    wrapper.appendChild(button);
+    }
+
+    console.log(hiddenItems);
+
+    // Responsive & humburger
+
+    const allBoxes = document.querySelectorAll(".box");
+    const sidebar = document.getElementById("sidebar");
+
+    allBoxes.forEach((box) => {
+    box.onclick = (e) => {
+        e.target.classList.toggle("active");
+        sidebar.style.display = e.target.classList.contains("active")
+        ? "flex"
+        : "none";
+
+        sidebar.classList.toggle("active");
+    };
+    });
+
+    window.onresize = (e) => {
+    const w = window.matchMedia("(max-width: 896px)");
+    if (!w.matches) {
+        sidebar.classList.add("active");
+        sidebar.style.display = "flex";
+        allBoxes.forEach(box => box.classList.add("active"))
+    } else {
+        sidebar.classList.remove("active");
+        sidebar.style.display = "none";
+        allBoxes.forEach(box => box.classList.remove("active"))
+    }
+    };
+</script>
 </html>
