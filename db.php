b1b6ba1b436c6c842c23bcff0d37640ef3ab5af9
cdricms <36056008+cdricms@users.noreply.github.com>
2023-12-17 14:56:49 +0100

    Docker working

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
-    $dbname = 'maths';
-    $dsn = "mysql:dbname=$dbname;host=localhost;charset=utf8";
-    $user = 'maths';
-    $password = '1234';
-    $db = new PDO($dsn, $user, $password);
+$dbname = 'maths';
+$dsn = "mysql:dbname=$dbname;host=db;port=3306;charset=utf8";
+$user = 'maths';
+$password = '1234';
+$db = new PDO($dsn, $user, $password);
 ?>
